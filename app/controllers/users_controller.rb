3f046c257f4d6ea5c00f80ea5c6a3429561d559b
掛野広貴 <ryokucha1173@gmail.com>
2018-11-17 21:22:06 +0900

    add-notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
 
-def edit
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+  end
 end
 
-def update
-  if current_user.update(user_params)
-    redirect_to root_path
-  else
-    render :edit
-end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 
-private
-
-def user_params
-  params.require(:user).permit(:name, :email)
-end
-
-end
 end
