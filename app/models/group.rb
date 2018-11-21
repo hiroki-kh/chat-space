12ea544513a19df07f6ee58289724fe41e98a549
掛野広貴 <ryokucha1173@gmail.com>
2018-11-21 18:04:59 +0900

    Edit-messages-view
    メッセージの送信機能と、メッセージのビューへの表示機能

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :members
   has_many :messages
   validates :name, presence: true
+
+  def show_last_message
+    if(last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだ投稿されていません'
+    end
+  end
 end
