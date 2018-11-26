72cd677560c01d00015f1bd1519890ef80165b26
掛野広貴 <ryokucha1173@gmail.com>
2018-11-27 01:58:39 +0900

    Add-factorygirl

---
+++ spec/factories/messages.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :message do
+    content Faker::Lorem.sentence
+    image File.open("#{Rails.root}/public/uploads/message/images/5/cat1.png")
+    user
+    group
+  end
+end
