95ce9abc61986538883a03039007ff35691f1f33
掛野広貴 <ryokucha1173@gmail.com>
2018-12-06 22:37:41 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,21 @@
 $(function() {
+  function buildHTML(message){
+    var html = `<div class="comment__member">
+                  ${message.user_name}
+                </div>
+                <div class="comment__time">
+                  ${message.created_at}
+                </div>
+                <div class="comment__text">
+                  <p class="message_content">
+                    ${message.content}
+                  </p>
+                  <p class="lower-message__image">
+                    ${if message.image}
+                  </p>
+                </div>`
+    return html;
+  }
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +28,13 @@ $(function() {
       processData: false,
       contentType: false,
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('#chat').append(html)
+      $('.comment-form').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
