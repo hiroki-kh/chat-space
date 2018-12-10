d37c5e494a6cf79c10e39f21d2aaa8e19394e673
掛野広貴 <ryokucha1173@gmail.com>
2018-12-10 20:07:10 +0900

    Add buildImageHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,8 +10,22 @@ $(function() {
                   <p class="message_content">
                     ${message.content}
                   </p>
+                </div>`
+    return html;
+  }
+  function buildImageHTML(message){
+    var html = `<div class"comment__member">
+                  ${message.user_name}
+                </div>
+                <div class="comment__time">
+                  ${message.created_at}
+                </div>
+                <div class="comment__text">
+                  <p class="message_content">
+                    ${message.content}
+                  </p>
                   <p class="lower-message__image">
-                    ${if message.image}
+                    <img src="${message.image.url}">
                   </p>
                 </div>`
     return html;
@@ -29,12 +43,20 @@ $(function() {
       contentType: false,
     })
     .done(function(data){
-      var html = buildHTML(data);
+      if (data.image.url == null) {
+        var html = buildHTML(data);
+      }
+      else {
+        var html = buildImageHTML(data);
+      }
       $('#chat').append(html)
-      $('.comment-form').val('')
+      $('.comment-form').val('');
+      $('#chat').animate({scrollTop:$('#chat')[0].scrollHeight});
+      $('.send-button').prop('disabled', false);
     })
     .fail(function(){
       alert('error');
     })
   })
 });
+
