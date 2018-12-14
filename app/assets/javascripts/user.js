10f471d279dbfe8d30484ff7d14246155efd0f28
掛野広貴 <ryokucha1173@gmail.com>
2018-12-14 21:27:35 +0900

    Add member-function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,6 @@
 $(function() {
   var search_list = $("#user-search-result");
+  var member_list = $("#chat-group-users")
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">
@@ -15,6 +16,15 @@ $(function() {
                   ${user}
                 </div>`
     search_list.append(html);
+  }
+
+  function appendMember(user) {
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+  <p class='chat-group-user__name'>${user.name}</p>
+  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+</div>`
+    member_list.append(html);
   }
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
@@ -39,8 +49,12 @@ $(function() {
     .fail(function() {
       alert('error');
     });
+    $(document).on('click',".chat-group-user__btn--add", function() {
+      var member = {};
+      member.id = $(this).attr('data-user-id');
+      member.name = $(this).attr('data-user-name');
+      appendMember(member);
+      $(this).parent().remove();
+    })
   });
-  $('#user-search-result').on('click','div', function() {
-    $(this).remove();
-  })
 });
