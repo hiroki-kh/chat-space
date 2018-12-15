2a987aa9bfe86103bacf8a9cdf484b676bb47614
掛野広貴 <ryokucha1173@gmail.com>
2018-12-15 16:45:15 +0900

    Change data name
    データの名前をわかりやすく変更

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 $(function() {
   var search_list = $("#user-search-result");
-  var member_list = $("#chat-group-users")
+  var member_list = $("#chat-group-users");
+
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">
@@ -31,7 +32,7 @@ $(function() {
     $.ajax({
       type: "GET",
       url: "/users",
-      data: {nameSearch: input},
+      data: {keyword: input},
       dataType: 'json',
     })
 
